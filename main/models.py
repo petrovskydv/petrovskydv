c181cc2e3082d310fdab77a265d40a87d03e5793
petrovskyd <petrovsky.dv@gmail.com>
2021-05-26 13:14:47 +0300

    create models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,72 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField("Заголовок", max_length=200)
+    slug = models.SlugField("Название в виде url", max_length=200)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, )
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def posts_number(self):
+        return self.sellers_posts.count()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="категория",
+        related_name='categories_posts'
+    )
+    seller = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        verbose_name="продавец",
+        related_name='sellers_posts'
+    )
+    tags = models.ManyToManyField(
+        "Tag",
+        related_name="posts",
+        verbose_name="Теги",
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'объявление'
+        verbose_name_plural = 'объявления'
+
+    def __str__(self):
+        return f'{self.title} - {str(self.seller)}'
+
+
+class Tag(models.Model):
+    title = models.CharField("Тег", max_length=20, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+    def __str__(self):
+        return self.title
