b9c3c8a896f551aa27c179b9e152436706767f16
petrovskyd <petrovsky.dv@gmail.com>
2021-06-01 15:34:25 +0300

    formatting code

--- main/views.py
+++ main/views.py
@@ -5,6 +5,6 @@ from bulletin_board.settings import MAINTENANCE_MODE
 
 def index(request):
     context = {
-        'maintenance_mode':MAINTENANCE_MODE,
+        'maintenance_mode': MAINTENANCE_MODE,
     }
     return render(request, 'index.html', context=context)
