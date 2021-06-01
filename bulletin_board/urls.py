9ccde8c5781cc5239835ccd3dd32d5187a4d4d76
petrovskyd <petrovsky.dv@gmail.com>
2021-06-01 16:43:11 +0300

    add detail views

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('cars', views.CarList.as_view(), name='cars'),
+    path('cars/<int:pk>/', views.CarDetailView.as_view(), name='cars-detail'),
     path('services', views.ServiceList.as_view(), name='services'),
+    path('services/<int:pk>/', views.ServiceDetailView.as_view(), name='services-detail'),
     path('items', views.PersonalItemList.as_view(), name='items'),
+    path('items/<int:pk>/', views.PersonalItemDetailView.as_view(), name='items-detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from bulletin_board.settings import MAINTENANCE_MODE
 from main.models import Car, Service, PersonalItem
@@ -16,9 +16,21 @@ class CarList(ListView):
     model = Car
 
 
+class CarDetailView(DetailView):
+    model = Car
+
+
 class ServiceList(ListView):
     model = Service
 
 
+class ServiceDetailView(DetailView):
+    model = Service
+
+
 class PersonalItemList(ListView):
+    model = PersonalItem
+
+
+class PersonalItemDetailView(DetailView):
     model = PersonalItem
