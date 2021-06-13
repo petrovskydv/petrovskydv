aeb07d9ad67ee8183530fd7b529f81e017b18eab
ДенНоут <pd@pravgorod.ru>
2021-06-13 17:44:11 +0300

    add notification of a new ad

--- main/signals/handlers.py
+++ main/signals/handlers.py
@@ -3,18 +3,29 @@ from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from main.models import PersonalItem, Subscriber
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         instance.groups.add(Group.objects.get(name='common users'))
-        send_email(instance)
+        text_content = 'Вы зарегистрированы на сайте.'
+        html_content = '<p>Вы зарегистрированы на сайте.</p>'
+        send_email(text_content, text_content, html_content, [instance.email])
 
 
-def send_email(user):
-    subject, from_email, to = 'hello', 'from@example.com', user.email
-    text_content = 'Вы зарегистрированы на сайте.'
-    html_content = '<p>Вы зарегистрированы на сайте.</p>'
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+@receiver(post_save, sender=PersonalItem)
+def notify_subscribers(sender, instance, created, **kwargs):
+    subscribers = Subscriber.objects.all()
+    recipients = [subscriber.user.email for subscriber in subscribers]
+    text_content = 'На сайте появилось новое объявление.'
+    html_content = f'<p>На сайте появилось новое объявление.</p> <a href={instance.get_absolute_url()}></a>'
+    send_email(text_content, text_content, html_content, recipients)
+
+
+def send_email(subject, text_content, html_content, recipients):
+    from_email = 'from@example.com'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, recipients)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
