794828099a93aedb8a796484c949f8b9e1ce478f
Денис Петровский <petrovsky.dv@gmail.com>
2021-06-20 13:09:22 +0300

    Install Redis as a message broker for celery

---
+++ bulletin_board/celery.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bulletin_board.settings')
+app = Celery('bulletin_board')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
